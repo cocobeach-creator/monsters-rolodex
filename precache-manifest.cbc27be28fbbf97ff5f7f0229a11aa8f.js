0668827aff1de2e88c2031dfed67756194609555
Carolina <carolinasantos@cosaia.com>
2020-03-20 19:43:32 +0530

    Updates

---
+++ precache-manifest.cbc27be28fbbf97ff5f7f0229a11aa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c981d036c1f9637d57c43946141c50d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3014720e20518b998f98",
+    "url": "/monsters-rolodex/static/css/main.4e3187a1.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3014720e20518b998f98",
+    "url": "/monsters-rolodex/static/js/main.dabd70be.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
